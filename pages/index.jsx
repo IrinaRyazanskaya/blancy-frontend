294ab2804b15030b6fd37ba2dc7d946f2805ada0
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-11-04 16:40:48 +0300

    Сверстала шапку для больших десктопов

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,6 @@ function Home() {
         <div className={styles.container}>
             <Head>
                 <title>Главная</title>
-                <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width,initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
