c7e7a8930c7261022127a329347902a32a544b38
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-10-20 22:17:37 +0000

    Поменяла путь до сборки, настроила CI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ import nextPlugin from "@next/eslint-plugin-next";
 
 export default defineConfig([
   {
-    ignores: [".next/**", "out/**", "*.config.{js,cjs,mjs}"],
+    ignores: [".next/**", "dist/**", "*.config.{js,cjs,mjs}"],
   },
   {
     files: ["**/*.{js,cjs,mjs}"],
