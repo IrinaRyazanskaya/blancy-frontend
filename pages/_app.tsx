d0a85c5bcb53e91b4d246540def14e2b9564584b
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-10-20 23:31:44 +0000

    Исправила пути до шрифтов

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
 import "../styles/globals.css";
 
+const circe = localFont({
+  variable: "--font-circe",
+  src: [
+    { path: "../public/fonts/circe.woff2", weight: "400", style: "normal" },
+    { path: "../public/fonts/circe.woff", weight: "400", style: "normal" },
+    { path: "../public/fonts/circe-light.woff2", weight: "300", style: "normal" },
+    { path: "../public/fonts/circe-light.woff", weight: "300", style: "normal" },
+    { path: "../public/fonts/circe-bold.woff2", weight: "700", style: "normal" },
+    { path: "../public/fonts/circe-bold.woff", weight: "700", style: "normal" },
+    { path: "../public/fonts/circe-extrabold.woff2", weight: "800", style: "normal" },
+    { path: "../public/fonts/circe-extrabold.woff", weight: "800", style: "normal" },
+  ],
+  display: "swap",
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={circe.variable}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
